0deb1e20210ef30ee4e52312e341513d34d678b9
cheertt <1913278504@qq.com>
2020-09-18 22:09:57 +0800

    2020-09-18

---
+++ src/huawei/hj61_放苹果.java
@@ -0,0 +1,21 @@
+package huawei;
+
+import java.util.Scanner;
+
+public class hj61_放苹果 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+
+    }
+
+    // 放苹果分为两种情况，一种是盘子为空，一种是每个盘子都有苹果
+    private static int count(int m, int n) {
+
+        return 0;
+    }
+}
